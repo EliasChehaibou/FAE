1ae67534489ee6b40fe2cf83b5d860849fa491e1
Elias Chehaibou <elia.chehaibou@gmail.com>
2022-02-28 11:07:37 +0100

    Barre de recherche

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,13 +5,18 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className='Home'>
-            <h1 className='titre'>Chez FAE on a tout ce qu'on veut !</h1>
-            <div className='rech'>
-                <p>Rechercher :</p>
-                <input/>
+            <div className='logo'>
+                <img src='/static/logo.png' alt="Foncez aux enchères" className='img'/>
             </div>
             
-           <Navbar/>
+            <h1 className='titre'>Chez FAE on a tout ce qu'on veut !</h1>
+            <Navbar/>
+            <div className='rech'>
+                <input className='input' placeholder='Rechercher ici ce que vous voulez...'/>
+                <button className='btn_rech'>Rechercher</button>
+            </div>
+            
+           
         </div>
     );
 };
