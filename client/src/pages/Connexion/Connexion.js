6956f81caa0ea3a2614e05fb3ddb33d4f99421b1
Ahmed-Amine01 <aminezitoun8@gmail.com>
2022-05-08 18:44:02 +0200

    fix bug page connexion

--- client/src/pages/Connexion/Connexion.js
+++ client/src/pages/Connexion/Connexion.js
@@ -1,57 +1,69 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../../Navbar/Navbar';
-import { connectUser } from '../../rest/search';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../Navbar/Navbar";
+import { connectUser } from "../../rest/search";
 
 const Connexion = () => {
-    const [IDUtilisateur, setIDUtilisateur] = useState([]);
+  const [IDUtilisateur, setIDUtilisateur] = useState([]);
 
+  let navigate = useNavigate();
+  function validateEmail(email) {
+    var re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
-    let navigate = useNavigate();
-    function validateEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+  function Verification(event) {
+    event.preventDefault();
+    // Récupérer la valeur du champ email
+    var Email = document.getElementById("email").value;
+
+    // Contrôle sur l'email
+    if (Email == "" || !validateEmail(Email)) {
+      document.getElementById("email").style.backgroundColor = "red";
+      document.getElementById("email").style.color = "#FFF";
+      return false;
     }
 
-    function Verification() {
-        // Récupérer la valeur du champ email
-        var Email = document.getElementById('email').value;
-
-        // Contrôle sur l'email
-        if(Email=='' || !validateEmail(Email)) {
-            document.getElementById('email').style.backgroundColor="red";
-            document.getElementById('email').style.color="#FFF";
-            return false;
-        }        
-        
-        var myForm = document.getElementById('form');
-        let formData = new FormData(myForm);
-        var object = {};
-        formData.forEach((value, key) => (object[key] = value));
-        connectUser(object).then((response) => {
-            console.log(IDUtilisateur);
-            setIDUtilisateur(response.data);
-            console.log(response.data);
-        });
-        console.log(IDUtilisateur);
-        if (IDUtilisateur) {
-            //navigate('/profil');
-        }
-        //window.location.reload();
-        
-    }
-    return (
-        <div>
-            <Navbar/>
-            <form id='form' name='form' action='http://localhost:2000/connexion' method='post' onSubmit={Verification} className='Form'>          
-                <label htmlFor="email">E-mail</label>
-                <input type="text" id="email" name="Email" required/>
-                <label htmlFor="password">Mot de passe</label>
-                <input type="password" id="password" name="Password" required/>
-                <input type='submit' value='Connexion'/>
-            </form>
-        </div>
-    );
+    var myForm = document.getElementById("form");
+    let formData = new FormData(myForm);
+    var object = {};
+    formData.forEach((value, key) => (object[key] = value));
+    connectUser(object).then((response) => {
+      console.log(response.status);
+      //   if (response.status == 200) {
+      //     window.location = "/login";
+      //     console.log("ok 200");
+      //   }
+      console.log(IDUtilisateur);
+      setIDUtilisateur(response.data);
+      console.log(response.data);
+    });
+    console.log(IDUtilisateur);
+    //if (IDUtilisateur) {
+    //navigate('/profil');
+    //}
+    //window.location.reload();
+  }
+  return (
+    <div>
+      <Navbar />
+      <form
+        id="form"
+        name="form"
+        method="post"
+        onSubmit={Verification}
+        className="Form"
+        action="http://localhost:2000/connexion"
+      >
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="Email" required />
+        <label htmlFor="password">Mot de passe</label>
+        <input type="password" id="password" name="Password" required />
+        <input type="submit" value="Connexion" />
+      </form>
+    </div>
+  );
 };
 
 export default Connexion;
--- server.js
+++ server.js
@@ -1,46 +1,50 @@
-var express = require('express')
+var express = require("express");
 
-require('dotenv').config()
+require("dotenv").config();
 
-var PORT = process.env.PORT || 7000
+var PORT = process.env.PORT || 2000;
 
-var cors = require('cors')
-var mysql = require('mysql');
+var cors = require("cors");
+var mysql = require("mysql");
 
-var connection
+var connection;
 
-var dbhost = process.env.DB_HOST || 'localhost'
-var dbuser = process.env.DB_USER || 'root'
-var dbpassword = process.env.DB_PASSWORD || ''
-var dbname = process.env.DB_NAME || 'fae_database'
-var dev_database_url = 'mysql://'+dbuser+':'+dbpassword+'@'+dbhost+'/'+dbname
+var dbhost = process.env.DB_HOST || "localhost";
+var dbuser = process.env.DB_USER || "root";
+var dbpassword = process.env.DB_PASSWORD || "";
+var dbname = process.env.DB_NAME || "ebey";
+var dev_database_url =
+  "mysql://" + dbuser + ":" + dbpassword + "@" + dbhost + "/" + dbname;
 
-
-const app = express()
+const app = express();
 
 function handleMysqlContext() {
-    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || dev_database_url)
-    connection.connect(function(err) {
-      if(err) {
-        console.log('error when connecting to db:', err);
-        setTimeout(handleMysqlContext, 2000);
-      }
-    });
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleMysqlContext();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the waittimeout
-        throw err;                                  // server variable configures this)
-      }
-    });
-  }
+  connection = mysql.createConnection(
+    process.env.CLEARDB_DATABASE_URL || dev_database_url
+  );
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleMysqlContext, 2000);
+    }
+  });
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleMysqlContext(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the waittimeout
+      throw err; // server variable configures this)
+    }
+  });
+}
 
-handleMysqlContext()
+handleMysqlContext();
 
-app.use(cors())
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
 /*app.get('/api/fae', (,res) => {
     res.send({
@@ -48,92 +52,104 @@ app.use(express.json())
     })
 })*/
 app.listen(PORT, () => {
-  console.log(`le serveur est lancé sur le port: ${PORT}`)
-})
-
+  console.log(`le serveur est lancé sur le port: ${PORT}`);
+});
 
 // recherche annonces
-  app.get('/search/annonces', function (req, res) {
-    var query = 'SELECT * from annonces'
-    connection.query(query,
-      function (error, results, fields) {
-      if (error) throw error;
-      res.send(JSON.stringify(results));
-      });
-  });
-
-// recherche annonces par categorie
-app.get('/search/annonces/categorie', function (req, res) {
-  var params = req.query;
-  var query = 'SELECT * from annonces where IDCategorie =' + connection.escape(params.IDCate)
-  connection.query(query,
-    function (error, results, fields) {
+app.get("/search/annonces", function (req, res) {
+  var query = "SELECT * from annonces";
+  connection.query(query, function (error, results, fields) {
     if (error) throw error;
     res.send(JSON.stringify(results));
-    });
+  });
+});
+
+// recherche annonces par categorie
+app.get("/search/annonces/categorie", function (req, res) {
+  var params = req.query;
+  var query =
+    "SELECT * from annonces where IDCategorie =" +
+    connection.escape(params.IDCate);
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+  });
 });
 
 // recherche annonces avec texte
-app.get('/search/annonces/recherche', function (req, res) {
+app.get("/search/annonces/recherche", function (req, res) {
   var params = req.query;
-  var query = "SELECT * from annonces where Titre LIKE '%" + params.Texte
-  query += "%'"
-  connection.query(query,
-    function (error, results, fields) {
+  var query = "SELECT * from annonces where Titre LIKE '%" + params.Texte;
+  query += "%'";
+  connection.query(query, function (error, results, fields) {
     if (error) throw error;
     res.send(JSON.stringify(results));
-    });
+  });
 });
 
 // recherche annonces avec texte et categorie
-app.get('/search/annonces/rechcate', function (req, res) {
+app.get("/search/annonces/rechcate", function (req, res) {
   var params = req.query;
-  var query = "SELECT * from annonces where Titre LIKE '%" + params.Texte
-  query += "%' AND IDCategorie =" + connection.escape(params.IDCate)
-  connection.query(query,
-    function (error, results, fields) {
+  var query = "SELECT * from annonces where Titre LIKE '%" + params.Texte;
+  query += "%' AND IDCategorie =" + connection.escape(params.IDCate);
+  connection.query(query, function (error, results, fields) {
     if (error) throw error;
     res.send(JSON.stringify(results));
-    });
+  });
 });
 
-  // recherche categories
-  app.get('/search/categories', function (req, res) {
-    var query = 'SELECT * from categories'
-    connection.query(query,
-      function (error, results, fields) {
+// recherche categories
+app.get("/search/categories", function (req, res) {
+  var query = "SELECT * from categories";
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+  });
+});
+
+// formulaire d'inscription
+app.post("/inscription", function (req, res) {
+  var params = req.body;
+  var userID;
+  if (params.Birthdate == "") {
+    delete params.Birthdate;
+  }
+  if (params.Confirm) {
+    delete params.Confirm;
+  }
+  connection.query(
+    "SELECT MAX(IDUtilisateur) AS IDUser from utilisateurs",
+    function (error, results, fields) {
       if (error) throw error;
+      userID = results[0].IDUser;
+      connection.query(
+        "INSERT INTO `utilisateurs` SET IDUtilisateur = " +
+          (userID + 1) +
+          ", ?",
+        params,
+        function (error, results, fields) {
+          if (error) throw error;
+          res.status(204).send();
+        }
+      );
+    }
+  );
+});
+
+// formulaire de connexion
+app.post("/connexion", function (req, res) {
+  var params = req.body;
+  console.log(params);
+  connection.query(
+    "SELECT IDUtilisateur from utilisateurs WHERE Email='" +
+      params.Email +
+      "' AND password='" +
+      params.Password +
+      "'",
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
       res.send(JSON.stringify(results));
-      });
-  });
-
-  // formulaire d'inscription
-  app.post('/inscription', function (req, res) {
-    var params = req.body;
-    var userID;
-    if (params.Birthdate==''){
-      delete params.Birthdate;
     }
-    if (params.Confirm){
-      delete params.Confirm;
-    }
-    connection.query('SELECT MAX(IDUtilisateur) AS IDUser from utilisateurs', function (error, results, fields) { 
-      if (error) throw error;
-      userID = results[0].IDUser; 
-      connection.query('INSERT INTO `utilisateurs` SET IDUtilisateur = '+(userID+1)+', ?', params, function (error, results, fields) { 
-        if (error) throw error;
-        res.status(204).send();
-      });
-    });
-  });
-
-    // formulaire de connexion
-    app.post('/connexion', function (req, res) {
-      var params = req.body;
-      console.log(params);
-      connection.query("SELECT IDUtilisateur from utilisateurs WHERE Email='"+params.Email+"' AND password='"+params.Password+"'", function (error, results, fields) { 
-        if (error) throw error;
-        console.log(results);
-        res.send(JSON.stringify(results));
-      });
-    });
+  );
+});
