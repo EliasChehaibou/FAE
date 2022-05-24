7030c29a3f5025d426b70aee7b44d416e3f42915
Ahmed-Amine01 <aminezitoun8@gmail.com>
2022-05-24 11:20:17 +0200

    css de annonce v1

--- client/src/Annonce/Annonce.js
+++ client/src/Annonce/Annonce.js
@@ -1,17 +1,45 @@
-import React from 'react';
-
-
+import React from "react";
+import "./Annonce.css";
+import logo from "./logo.png";
 const Annonce = (props) => {
-
+  console.log(props.data);
+  function setDate(annonceDate) {
+    var myDate = new Date(annonceDate);
     return (
-        <div>
-            <div>Titre de l'annonce: <a href={'./detail?ida='+props.data.IDAnnonce}>{props.data.Titre}</a></div>
-            <div>ID de l'annonce: {props.data.IDAnnonce}</div>
-            <div>ID de l'utilisateur: {props.data.IDUtilisateur}</div>
-            <div >Date de creation: {props.data.DateCrea}</div>
-            
-        </div>
+      myDate.getMonth() +
+      1 +
+      "/" +
+      myDate.getDate() +
+      "/" +
+      myDate.getFullYear() +
+      " à " +
+      myDate.getHours() +
+      " heures et " +
+      myDate.getMinutes() +
+      " minutes."
     );
+  }
+
+  return (
+    <div className="container">
+      <div className="photo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="informations">
+        <div>
+          <div>
+            Titre de l'annonce:{" "}
+            <a href={"./detail?ida=" + props.data.IDAnnonce}>
+              {props.data.Titre}
+            </a>
+          </div>
+          <div>Enchere acttuelle: {props.data.Enchere}</div>
+          <div>Achat immédiat: {props.data.Enchere}</div>
+          <div>Fin le: {setDate(props.data.DateFin)}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Annonce;
